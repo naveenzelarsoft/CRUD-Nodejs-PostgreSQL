412394060ddc94ae2b41a682d7b08957c323e700
naveen <naveen@gmail.com>
2021-08-27 18:44:27 +0530

    updated

--- table.sql
+++ table.sql
@@ -5,5 +5,4 @@ DROP DATABASE IF EXISTS testdb;
 CREATE DATABASE testdb;
 
 \c testdb;
-
 CREATE TABLE employee (id serial NOT NULL,name character varying(200) NOT NULL,job character varying(200) NOT NULL,department character varying(20) NOT NULL,salary int NOT NULL,hire_date date NOT NULL);
