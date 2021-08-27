44b337df72112ca031091a7b3e5906febc00a83f
naveen <naveen@gmail.com>
2021-08-27 12:01:04 +0530

    postgres

--- table.sql
+++ table.sql
@@ -1,1 +1,7 @@
+DROP DATABASE IF EXISTS testdb;
+
+CREATE DATABASE testdb;
+
+\c testdb;
+
 CREATE TABLE employee (id serial NOT NULL,name character varying(200) NOT NULL,job character varying(200) NOT NULL,department character varying(20) NOT NULL,salary int NOT NULL,hire_date date NOT NULL);
