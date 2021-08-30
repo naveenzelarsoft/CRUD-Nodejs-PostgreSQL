276d0b9c66bde7a4895ea64689095c2d1c8ed61e
naveen <naveen@gmail.com>
2021-08-30 11:45:08 +0530

    update

--- routes/employees.js
+++ routes/employees.js
@@ -1,5 +1,5 @@
 const { Client } = require('pg');
-var connectionString = "postgres://postgres:postgres@localhost:5432/testdb";
+var connectionString = DATABASE_CONNECTIONSTRING;
 
 const client = new Client({
     connectionString: connectionString
